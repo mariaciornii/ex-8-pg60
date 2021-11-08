27c03f93b477275f47b1f53e8173093ba9a95c23
mariaciornii <70654766+mariaciornii@users.noreply.github.com>
2021-11-08 10:41:18 +0200

    Add files via upload

---
+++ problema 8 pg 60.py
@@ -0,0 +1,7 @@
+A= {1, 4, 7}
+B= {2, 4, 9 }
+print (A)
+print ( B)
+print ( 'Reumiunea multimilor->', A.union (B))
+print ( 'Intersectia multimilor->' ,A.intersection (B))
+print ( 'Diferenta multimilor ->', A.difference (B))
