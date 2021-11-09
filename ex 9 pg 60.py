b60f31b216f8666b4af9b87f406e9ca381a5edfb
mariaciornii <70654766+mariaciornii@users.noreply.github.com>
2021-11-09 18:37:27 +0200

    Add files via upload

---
+++ ex 9 pg 60.py
@@ -0,0 +1,7 @@
+A= { A, C, D, T, U }
+B= { Z, E, L, G, H }
+print (A)
+print ( B)
+print ( 'Reumiunea multimilor->', A.union (B))
+print ( 'Intersectia multimilor->', A.intersection (B))
+print ( 'Diferenta multimilor ->', A.difference (B))
